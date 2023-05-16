f01a5f714132d47d1566fdc617c790a3a3e51d78
Mindru-Bogdan <77194184+Mindru-Bogdan@users.noreply.github.com>
2023-05-16 19:13:47 +0300

    CSS Solution Desktop

---
+++ src/Components/Img.js
@@ -0,0 +1,21 @@
+import PatternDesktop from '../../src/bg-pattern-desktop.svg';
+import PatternMobile from '../../src/bg-pattern-mobile.svg';
+import IllustrationWomanDesktop from '../../src/illustration-woman-online-desktop.svg';
+import IllustrationWomanMobile from '../../src/illustration-woman-online-mobile.svg';
+import IllustrationBox from '../../src/illustration-box-desktop.svg';
+
+import classes from './Img.module.css';
+
+const Img = () => {
+  return (
+    <div>
+      <img className={classes.patternDesktop} src={PatternDesktop} />
+      {/* <img className={classes.patternMobile} src={PatternMobile} /> */}
+      <img className={classes.womanDesktop} src={IllustrationWomanDesktop} />
+      {/* <img className={classes.womanMobile} src={IllustrationWomanMobile} /> */}
+      <img className={classes.box} src={IllustrationBox} />
+    </div>
+  );
+};
+
+export default Img;
